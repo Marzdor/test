a0a46c774aadfd319ebc8da21f28200114056361
Max <marzdor@gmail.com>
2022-03-08 15:04:34 -0800

    added todo model added todo controller added todo views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -18,9 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todos', function () {
-    return view('todos');
-});
+Route::resource('/todos', TodoController::class);
 
 Route::get('/calendar', function () {
     return view('calendar');
